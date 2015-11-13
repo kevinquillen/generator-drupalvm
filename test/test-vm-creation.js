75b3f04a58b0b95a3592285d29251d1d31d8f28a
Kevin Quillen <kevinquillen@gmail.com>
2015-11-13 16:07:00 -0500

    Add tests

---
+++ test/test-vm-creation.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var helpers = yeoman.test;
+var assert = yeoman.assert;
+
+describe('backbone:app', function () {
+  describe('when using require.js', function () {
+    before(function () {
+      // Mock the options, set up an output folder and run the generator
+    });
+
+    it('generate a router.js file', function () {
+      // assert the file exist
+      // assert the file uses AMD definition
+    });
+  });
+});
