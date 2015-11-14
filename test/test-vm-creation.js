82f5fd726121e51793003a812733921ede480f6f
Kevin Quillen <kevinquillen@gmail.com>
2015-11-14 15:53:45 -0500

    Add a test that verifies config.yml is written correctly with the supplied input

--- test/test-vm-creation.js
+++ test/test-vm-creation.js
@@ -5,15 +5,51 @@ var yeoman = require('yeoman-generator');
 var helpers = yeoman.test;
 var assert = yeoman.assert;
 
-describe('backbone:app', function () {
-  describe('when using require.js', function () {
-    before(function () {
-      // Mock the options, set up an output folder and run the generator
-    });
+var expectedFiles = ['.gitignore', 'tools/drupalvm/config.yml'];
 
-    it('generate a router.js file', function () {
-      // assert the file exist
-      // assert the file uses AMD definition
+describe('drupalvm:app', function () {
+  describe('generates a config.yml with user input', function () {
+    it('creates config.yml with expected values', function (done) {
+      var vm_root = path.join(__dirname, './temp') + '/tools/drupalvm';
+      var vm_config = vm_root + '/config.yml';
+
+      helpers.run(path.join(__dirname, '../app'))
+        .inDir(path.join(__dirname, './temp'))
+        .withOptions({
+          'skip-install': false
+        })
+        .withPrompts({
+          'install_drupal': true,
+          'drupal_version': '7',
+          'vagrant_hostname': 'mochatest',
+          'vagrant_machine_name': 'mochatest',
+          'drupalvm_webserver': 'nginx',
+          'packages': ['adminer', 'pimpmylog', 'solr', 'xdebug'],
+          'vagrant_ip': '0.0.0.0',
+          'sync_type': 'rsync',
+          'vagrant_memory': 1024,
+          'vagrant_cpus': 1,
+          'php_version': '5.6',
+          'php_memory_limit': 128,
+        })
+        .on('end', function () {
+          assert.file(expectedFiles);
+          assert.fileContent(vm_config, /install_site: true/);
+          assert.fileContent(vm_config, /vagrant_hostname: mochatest/);
+          assert.fileContent(vm_config, /vagrant_machine_name: mochatest/);
+          assert.fileContent(vm_config, /drupalvm_webserver: nginx/);
+          assert.fileContent(vm_config, /vagrant_ip: 0.0.0.0/);
+          assert.fileContent(vm_config, /type: rsync/);
+          assert.fileContent(vm_config, /vagrant_memory: 1024/);
+          assert.fileContent(vm_config, /vagrant_cpus: 1/);
+          assert.fileContent(vm_config, /php_version: "5.6"/);
+          assert.fileContent(vm_config, /- adminer/);
+          assert.fileContent(vm_config, /- pimpmylog/);
+          assert.fileContent(vm_config, /- solr/);
+          assert.fileContent(vm_config, /- xdebug/);
+          assert.fileContent(vm_config, /#- nodejs/);
+          done();
+        });
     });
   });
 });
